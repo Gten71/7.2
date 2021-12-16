d21b9431475fc3a88b15328487976cd7f2769e1f
Максим Реснянский <mresnyansky@gmail.com>
2021-12-16 11:04:03 +0200

    Add project files.

---
+++ 7.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            var res = Console.ReadLine();
+            var length = res.Length;
+            var a = "";
+            for (int i = length; i >= 1; i--)
+            {
+                a += res[i - 1];
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
